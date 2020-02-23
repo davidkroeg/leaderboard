9d0a9fa1cadc56cd3858def0280099d13a9ef3dc
David Krög <david_@gmx.net>
2020-02-23 14:34:58 +0100

    fix: updated indent

--- Leaderboard/Data/APIClient.swift
+++ Leaderboard/Data/APIClient.swift
@@ -11,67 +11,66 @@ import Foundation
 let apiUrl = "http://www.dota2.com/webapi/ILeaderboard/GetDivisionLeaderboard/v0001?division=europe&leaderboard=2"
 
 class APIClient: NSObject {
-  
-  let decoder = JSONDecoder()
-  
-  override init() {
-    super.init()
     
-    decoder.keyDecodingStrategy = .convertFromSnakeCase
-  }
-  
-  //FIXME: - cant get players like this. Need to decode from leaderboard key
-  func fetchPlayers(completion: @escaping ([Player]?) -> Void) {
-    //fetch json and decode and update some array property
-    guard let url = URL(string: apiUrl) else {
-      print("Error unwraping url.")
-      return
-
-    }
-    URLSession.shared.dataTask(with: url) { (data, resp, err) in
-      
-      guard let unwrappedData = data else  {
-        print("Error getting data")
-        return
-      }
-      
-      do {
-        let players: [Player] = try self.decoder.decode([Player].self, from: unwrappedData)
-        completion(players)
-      } catch {
-        completion(nil)
-        print("Error getting API data: \(error.localizedDescription)")
-      }
-      
+    let decoder = JSONDecoder()
+    
+    override init() {
+        super.init()
         
-    }.resume()
-  }
-  
-  func fetchLeaderboard(completion: @escaping (Leaderboard?) -> Void) {
-    //fetch json and decode and update some array property
-    guard let url = URL(string: apiUrl) else {
-      print("Error unwraping url.")
-      return
-      
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
     }
-    URLSession.shared.dataTask(with: url) { (data, resp, err) in
-      
-      guard let unwrappedData = data else  {
-        print("Error getting data")
-        return
-      }
-      
-      do {
-        let leaderboard: Leaderboard = try self.decoder.decode(Leaderboard.self, from: unwrappedData)
-        completion(leaderboard)
-        print("Successfully retreived data")
-      } catch {
-        completion(nil)
-        print("Error getting API data: \(error)")
-      }
-      
-      
-    }.resume()
-  }
-  
+    
+    //FIXME: - cant get players like this. Need to decode from leaderboard key
+    func fetchPlayers(completion: @escaping ([Player]?) -> Void) {
+        //fetch json and decode and update some array property
+        guard let url = URL(string: apiUrl) else {
+            print("Error unwraping url.")
+            return
+            
+        }
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            
+            guard let unwrappedData = data else  {
+                print("Error getting data")
+                return
+            }
+            
+            do {
+                let players: [Player] = try self.decoder.decode([Player].self, from: unwrappedData)
+                completion(players)
+            } catch {
+                completion(nil)
+                print("Error getting API data: \(error.localizedDescription)")
+            }
+            
+            
+        }.resume()
+    }
+    
+    func fetchLeaderboard(completion: @escaping (Leaderboard?) -> Void) {
+        guard let url = URL(string: apiUrl) else {
+            print("Error unwraping url.")
+            return
+            
+        }
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            
+            guard let unwrappedData = data else  {
+                print("Error getting data")
+                return
+            }
+            
+            do {
+                let leaderboard: Leaderboard = try self.decoder.decode(Leaderboard.self, from: unwrappedData)
+                completion(leaderboard)
+                print("Successfully retreived data")
+            } catch {
+                completion(nil)
+                print("Error getting API data: \(error)")
+            }
+            
+            
+        }.resume()
+    }
+    
 }
